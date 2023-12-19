4851ba882ad8eee5ee8acaf6028a59e867ba7f56
Priyanka <pbhaukajee@gmail.com>
2023-12-19 11:39:12 -0600

    create operate function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, num1, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+      result = "Invalid";
+      break;
+  }
+  return result;
+}
+
+console.log(operate("*", 2, 5));
