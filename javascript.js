8f4ee09cc0f7118e9ba8bb8cb74ba7eedc5c8e9e
Priyanka <pbhaukajee@gmail.com>
2023-12-20 12:32:28 -0600

    update calculator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,62 @@
+const input = document.querySelector(".input");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equals");
+
+let firstNum = "";
+let secondNum = "";
+let operatorClicked = "";
+let calculation;
+
+//Show numbers in display & store the values
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    input.textContent += number.textContent;
+    if (operatorClicked === "") {
+      firstNum += number.textContent;
+    } else {
+      secondNum += number.textContent;
+    }
+  });
+});
+
+//Select an operator
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (secondNum == "") {
+      operatorClicked = operator.textContent;
+      input.textContent += operator.textContent;
+    } else {
+      calculation = operate(
+        operatorClicked,
+        parseFloat(firstNum),
+        parseFloat(secondNum)
+      );
+      updateDisplay(calculation);
+    }
+  });
+});
+
+//Show the result
+equal.addEventListener("click", function () {
+  calculation = operate(
+    operatorClicked,
+    parseFloat(firstNum),
+    parseFloat(secondNum)
+  );
+  updateDisplay(calculation);
+  firstNum = calculation;
+});
+
+//Show the result
+function updateDisplay(outcome) {
+  firstNum = "";
+  secondNum = "";
+  operatorClicked = "";
+  input.textContent = outcome;
+}
+
+//Math formula
 function add(a, b) {
   return a + b;
 }
@@ -14,6 +73,7 @@ function divide(a, b) {
   return a / b;
 }
 
+//Utilize the formula to get the result
 function operate(operator, num1, num2) {
   let result;
   switch (operator) {
